75450c55faa8aa7e67c98855bf7cf1acb1a7f7e5
Arthur Chen <crh0716@gmail.com>
2014-05-28 16:15:24 +0800

    Bug 1016280 - Do nothing if no section specified

--- apps/settings/js/settings.js
+++ apps/settings/js/settings.js
@@ -290,6 +290,7 @@ var Settings = {
           // If there isn't a section specified,
           // simply show ourselve without making ourselves a dialog.
           Settings._currentActivity = null;
+          return;
         }
 
         // Validate if the section exists
